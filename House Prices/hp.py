0f06005d27f8ba5f0ccc328bc9f0b0c9318dd6f7
ds18b20 <858362490@qq.com>
2018-10-21 01:19:30 +0900

    2018-10-21 add weight decay=1e-6 but loss rised from 0.14 to 0.18

--- House Prices/hp.py
+++ House Prices/hp.py
@@ -16,7 +16,7 @@ import time
 
 class MultiLayerRegression(object):
     def __init__(self, input_size, hidden_size_list, output_size,
-                 activation='relu', weight_init_std='relu', weight_decay_lambda=0):
+                 activation='relu', weight_init_std='relu', weight_decay_lambda=0.0):
         self.input_size = input_size
         self.output_size = output_size
         self.hidden_size_list = hidden_size_list
@@ -71,8 +71,12 @@ class MultiLayerRegression(object):
 
     def loss(self, x_batch, t_batch):
         y_batch = self.predict(x_batch)
-        loss = self.last_layer.forward(y_batch, t_batch)
-        return loss
+        weight_decay = 0
+        for idx in range(1, self.hidden_layer_num + 2):
+            W = self.params['W' + str(idx)]
+            weight_decay += 0.5 * self.weight_decay_lambda * np.sum(W ** 2)
+
+        return self.last_layer.forward(y_batch, t_batch) + weight_decay
 
     def gradient(self, x_batch, t_batch):
         # forward
@@ -87,7 +91,7 @@ class MultiLayerRegression(object):
             dout = layer.backward(dout)
         grad = {}
         for idx in range(1, len(self.hidden_size_list) + 2):
-            grad['W'+str(idx)] = self.layers['Affine' + str(idx)].d_W
+            grad['W' + str(idx)] = self.layers['Affine' + str(idx)].d_W + self.weight_decay_lambda * self.layers['Affine' + str(idx)].W
             grad['b'+str(idx)] = self.layers['Affine' + str(idx)].d_b
         return grad
     
@@ -117,19 +121,21 @@ if __name__ == '__main__':
     hp_data = HousePrices('./data')
     x, t, x_submission = hp_data.load()
     print('x.shape:', x.shape)
-    train_num = 1459
+    train_num = 1000
     train_x, train_y, test_x, test_y = x[:train_num, :], t[:train_num, :], x[train_num:, :], t[train_num:, :]
 
-    max_iterations = 2000
+    max_iterations = 20000
     batch_size = 128
     # initialize network optimizer
     weight_init_types = {'std=0.01': 0.01, 'Xavier': 'sigmoid', 'He': 'relu'}
     # optimizer = SGD(lr=0.01)
-    optimizer = Adam(lr=0.01)
+    optimizer = Adam(lr=1e-4)
 
-    network = MultiLayerRegression(input_size=331, hidden_size_list=[100, 100, 100, 10],
-                                   output_size=1, weight_init_std='sigmoid', activation='relu')
+    network = MultiLayerRegression(input_size=331, hidden_size_list=[100, 100, 100, 10], output_size=1,
+                                   weight_init_std='sigmoid', activation='relu',
+                                   weight_decay_lambda=1e-6)
     train_loss = []
+    test_loss = []
 
     # 2:訓練の開始==========
     for i in range(max_iterations):
@@ -138,29 +144,44 @@ if __name__ == '__main__':
         grads = network.gradient(x_batch, t_batch)
         optimizer.update(network.params, grads)
 
-        loss = network.loss(x_batch, t_batch)
-        train_loss.append(loss)
-        if i % 100 == 0:
+        tmp_train_loss = network.loss(x_batch, t_batch)
+        train_loss.append(tmp_train_loss)
+        tmp_test_loss = network.loss(test_x, test_y)
+        test_loss.append(tmp_test_loss)
+        if i % 1000 == 0:
             print("===========" + "iteration:" + str(i) + "===========")
-            # loss = network.loss(x_batch, t_batch)
-            print('Sigmoid-Xavier' + ":" + str(loss))
+            print('Train loss:', tmp_train_loss)
+            print('Test loss:', tmp_test_loss)
     # x_pre = train_x[0:3]
     # t_pre = train_y[0:3]
     # y_pre = network.predict(x_pre)
     # print('label:', t_pre)
     # print('prediction:', y_pre)
 
+    # generate submission csv
     y_submission = 10 ** network.predict(x_submission)
     generate_submission_csv(id_column=hp_data.test_id, predict_y=y_submission)
 
-    bins_range = 30
-    for idx, key in enumerate(network.activation_dict.keys()):
-        ax = plt.subplot(1, network.hidden_layer_num, idx + 1)
-        ax.set_title(key)
-        ran = (0, 1)
-        ax.hist(network.activation_dict[key].flatten(), bins=bins_range, range=ran)
-        if idx != 0:
-            plt.yticks([], [])
+    # # show activation layer out
+    # bins_range = 30
+    # for idx, key in enumerate(network.activation_dict.keys()):
+    #     ax = plt.subplot(1, network.hidden_layer_num, idx + 1)
+    #     ax.set_title(key)
+    #     ran = (0, 1)
+    #     ax.hist(network.activation_dict[key].flatten(), bins=bins_range, range=ran)
+    #     if idx != 0:
+    #         plt.yticks([], [])
+    #
+    # plt.tight_layout()
+    # plt.show()
 
-    plt.tight_layout()
+    # show train_loss & test_loss
+    markers = {'train_loss': 'o', 'test_loss': 's'}
+    x = np.arange(max_iterations)
+    plt.plot(x, train_loss, marker=markers['train_loss'], markevery=100, label='train_loss')
+    plt.plot(x, test_loss, marker=markers['test_loss'], markevery=100, label='test_loss')
+    plt.xlabel("iterations")
+    plt.ylabel("loss")
+    # plt.ylim(0, 1)
+    plt.legend()
     plt.show()
